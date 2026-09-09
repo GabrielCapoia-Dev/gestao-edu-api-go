9b25b329083fae4d06184f510e93ea11894e2bf2
GabrielCapoia-Dev <gabrielcapoia11@gmail.com>
2026-09-09 16:58:03 -0300

    feat: adicionar importação de crypto/rand e inicializar salt na função HashPassword

--- internal/security/password/password.go
+++ internal/security/password/password.go
@@ -1,7 +1,16 @@
 package password
 
-import "golang.org/x/crypto/argon2"
-
+import (
+	"golang.org/x/crypto/argon2"
+	"crypto/rand"
+)
 func HashPassword(password string) (string, error) {
+
+	// Gera um salt aleatório de 16 bytes
+	salt := make([]byte, 16)
+
+
+
 	return "", nil
 }
+
