f7027237e4a602f7ef71184a0782d4e780a4870d
GabrielCapoia-Dev <gabrielcapoia11@gmail.com>
2026-09-09 17:00:14 -0300

    feat: corrigir a função HashPassword para retornar o salt e o hash do password

--- internal/security/password/password.go
+++ internal/security/password/password.go
@@ -8,9 +8,17 @@ func HashPassword(password string) (string, error) {
 
 	// Gera um salt aleatório de 16 bytes
 	salt := make([]byte, 16)
+	_, err := rand.Read(salt)
 
+	// Verifica se ocorreu algum erro ao gerar o salt
+	if err != nil {
+		return "", err
+	}
 
+	// Hash o password com o salt
+	hash := argon2.IDKey([]byte(password), salt, 1, 64*1024, 4, 32)
 
-	return "", nil
+	// Retorna o salt e o hash
+	return string(salt) + ":" + string(hash), nil
 }
 
