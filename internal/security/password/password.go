d9eb3d88bd4426443f6984b715af3338afc8bbdb
GabrielCapoia-Dev <gabrielcapoia11@gmail.com>
2026-09-09 17:19:23 -0300

    feat: corrigir a função HashPassword para retornar o salt e o hash codificados em base64

--- internal/security/password/password.go
+++ internal/security/password/password.go
@@ -1,8 +1,9 @@
 package password
 
 import (
-	"golang.org/x/crypto/argon2"
 	"crypto/rand"
+	"encoding/base64"
+	"golang.org/x/crypto/argon2"
 )
 func HashPassword(password string) (string, error) {
 
@@ -16,9 +17,21 @@ func HashPassword(password string) (string, error) {
 	}
 
 	// Hash o password com o salt
+	/*
+		[]byte(password) → senha em bytes
+		salt             → salt aleatório
+		1                → número de iterações
+		64*1024          → memória: 64 MiB
+		4                → paralelismo
+		32               → tamanho final do hash: 32 bytes
+	*/
 	hash := argon2.IDKey([]byte(password), salt, 1, 64*1024, 4, 32)
 
+	// Codifica o salt e o hash em base64 para armazenar como string evitando erro de caracteres quebrados e bytes incorretos
+	saltEncoded := base64.RawStdEncoding.EncodeToString(salt)
+	hashEncoded := base64.RawStdEncoding.EncodeToString(hash)
+
 	// Retorna o salt e o hash
-	return string(salt) + ":" + string(hash), nil
+	return saltEncoded + ":" + hashEncoded, nil
 }
 
